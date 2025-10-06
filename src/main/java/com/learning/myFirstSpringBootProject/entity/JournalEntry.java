2989a0e73dbdb038dc7320fcafd89d6e711d09ea
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-10-06 23:43:03 +0530

    Get, Post, Put, Delete

--- src/main/java/com/learning/myFirstSpringBootProject/entity/JournalEntry.java
+++ src/main/java/com/learning/myFirstSpringBootProject/entity/JournalEntry.java
@@ -4,6 +4,15 @@ public class JournalEntry {
     private long id;
     private String title;
     private String content;
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 
     public String getTitle() {
         return title;
